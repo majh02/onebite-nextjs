6e8bb4ef38b4ebfe8ec3d5b56851124a0a6643a9
마지혜(AX기술개발팀) <jihye.ma@kt.com>
2025-02-27 14:59:07 +0900

    ⚡️ 페이지라우팅 실습

---
+++ onebite-next-study/section02/src/components/searchable-layout.tsx
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+import { ReactNode, useEffect, useState } from "react";
+
+export default function SearchableLayout({ children }:{
+    children: ReactNode;
+}) {
+    const router = useRouter();
+    const [search, setSearch] = useState("");
+
+    const q = router.query.q as string;
+
+    useEffect(() => {
+        setSearch(q || "");
+    }, [q])
+
+    const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    };
+
+    const onSubmit = () => {
+        if(!search) return;
+        router.push(`./search?q=${search}`);
+    }
+
+    return (
+        <div>
+            <div>
+                <input 
+                    value = {search}
+                    placeholder="검색어를 입력하세요 ..."
+                    onChange={onChangeSearch}
+                />
+                <button onClick={onSubmit}>검색</button>
+            </div>
+            {children}
+        </div>
+    );
+}
